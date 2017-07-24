2e0a437fd00fc2ec29ea3f4f302449aecc469851
ilakya96 <ilakyaselvi1996@gmail.com>
2017-07-24 15:27:08 +0530

    Create Holiday_working.java

---
+++ Holiday_working.java
@@ -0,0 +1,13 @@
+import java.util.*:
+public class Holiday_working
+{
+  public static void main(String args[])
+  {
+   Scanner sc=new Scanner(System.in);
+   String str=sc.next();
+   if(str.equalsIgnoreCase("sunday"))
+   System.out.println("false");
+   else
+   System.out.println("true");
+  }
+}
